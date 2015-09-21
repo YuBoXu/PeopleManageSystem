dc52f92b84a1843590f7e037b79912ff90979e79
xyq <xiongyuqiong1995@126.com>
2015-09-21 10:08:09 +0800

    入职员工查询

--- src/com/csu/servlet/xyq/YgxxServlet.java
+++ src/com/csu/servlet/xyq/YgxxServlet.java
@@ -3,6 +3,7 @@ package com.csu.servlet.xyq;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -41,7 +42,16 @@ public class YgxxServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		YgxxBiz biz=new YgxxBiz();
-		List<HashMap<String,String>> list=biz.find(request.getParameterMap());
+		List <HashMap<String,String>> list=biz.find(request.getParameterMap());
+		if (!list.isEmpty()){
+			Map<String,String> map = list.get(0);
+			request.getSession().setAttribute("ygxx", map);
+			response.sendRedirect("ygxx.jsp");
+		}
+		
+		
+		
+		
 	}
 
 }
